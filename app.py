601d7f3fd6787d05bd7dc0890101f6d4066eacd4
flavio <flavio.00000844530@unicap.br>
2024-10-14 13:06:25 -0300

    atualizando o ambiente virtual

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
