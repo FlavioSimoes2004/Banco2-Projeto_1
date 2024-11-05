95128f7d890ec7f58dd2c3d0197fd157242b9aec
Grimaldi-Joao <joaogrimaldi6@gmail.com>
2024-11-05 09:58:41 -0300

    Criação do usuario gerente e criação do Trigger

--- MariaDB-Queries/Users.sql
+++ MariaDB-Queries/Users.sql
@@ -2,6 +2,8 @@ CREATE USER 'Administrador'@'localhost' IDENTIFIED BY '1234';
 GRANT ALL PRIVILEGES ON Restaurante.* TO 'Administrador'@'localhost';
 
 CREATE USER 'Gerente'@'localhost' IDENTIFIED BY '1234';
+GRANT SELECT, UPDATE, DELETE ON Restaurante.* TO 'Gerente'@'localhost';
+FLUSH PRIVILEGES;
 
 CREATE USER 'Funcionario'@'localhost' IDENTIFIED BY '1234';
 GRANT INSERT, SELECT ON Restaurante.venda TO 'Funcionario'@'localhost';
